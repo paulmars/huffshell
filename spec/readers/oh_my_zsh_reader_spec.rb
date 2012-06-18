4d9868129a9295b1f69e411ab4222c086b65b5c3
Paul McKellar <paul.mckellar@gmail.com>
2012-06-18 12:37:54 -0700

    take in comments and removes leading whitespace

--- spec/readers/oh_my_zsh_reader_spec.rb
+++ spec/readers/oh_my_zsh_reader_spec.rb
@@ -1,18 +1,44 @@
 require 'spec_helper'
 
 describe OhMyZshReader do
-  before :each do
-zsh_history = <<-ZSHHISTORY
+  context "clean history" do
+    before :each do
+clean_history = <<-ZSHHISTORY
 : 1319231772:0;ls -l
 : 1319231998:0;git status
 ZSHHISTORY
 
-    file = stub(:read => zsh_history)
-    File.stub!(:open).and_return(file)
+      file = stub(:read => clean_history)
+      File.stub!(:open).and_return(file)
+    end
+
+    it "returns two commands" do
+      reader = OhMyZshReader.new("fake_file_name")
+      reader.shell_commands.size.should == 2
+    end
   end
 
-  it "returns two commands" do
-    reader = OhMyZshReader.new("fake_file_name")
-    reader.shell_commands.size.should == 2
+  context "with bad lines in history" do
+    before :each do
+dirty_history = <<-ZSHHISTORY
+: 1319231772:0;   ls -l
+: 1319231998:0;git status
+: 1319231998:0;# comment
+: 1319231998:0;git status
+ZSHHISTORY
+
+      file = stub(:read => dirty_history)
+      File.stub!(:open).and_return(file)
+    end
+
+    it "returns two commands" do
+      reader = OhMyZshReader.new("fake_file_name")
+      reader.shell_commands.size.should == 4
+    end
+
+    it "removes leading white space" do
+      reader = OhMyZshReader.new("fake_file_name")
+      reader.shell_commands.first[0].should_not == " "
+    end
   end
 end
