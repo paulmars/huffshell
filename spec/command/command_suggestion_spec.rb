f54c0faf21796b5f8061eb7aa115187676132719
Paul McKellar <paul.mckellar@gmail.com>
2012-06-20 09:56:59 -0700

    increase code coverage

--- spec/command/command_suggestion_spec.rb
+++ spec/command/command_suggestion_spec.rb
@@ -4,8 +4,10 @@ describe CommandSuggestion do
   it "create with list of words" do
     cs = CommandSuggestion.new(["git", "commit", "-m"])
     cs.abbreviation.should == "gcm"
+    cs.to_s.should == "'git commit -m' => \e[0;32;49mgcm\e[0m"
 
     cs = CommandSuggestion.new(["ls", "-l"])
     cs.abbreviation.should == "ll"
+    cs.to_s.should == "'ls -l' => \e[0;32;49mll\e[0m"
   end
 end
