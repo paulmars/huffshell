978bfc9efbfdf23c36ad63530763171eb26e3018
Paul McKellar <paul.mckellar@gmail.com>
2012-06-20 11:48:36 -0700

    inspect the environment for binarys, don't shell out

--- lib/shell/binary_checker.rb
+++ lib/shell/binary_checker.rb
@@ -5,7 +5,10 @@ class BinaryChecker
 
   def exist?
     # would love something better
-    out = `command -v #{@command} >/dev/null 2>&1`
-    $?.to_s.scan("0") != []
+    # out = `command -v #{@command} >/dev/null 2>&1`
+    # $?.to_s.scan("0") != []
+    !ENV['PATH'].split(':').
+        each {|folder| puts File.exists?(folder+'/'+@command)}.
+        inject(false){|a,b| a || b }
   end
 end
