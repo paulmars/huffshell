27a295dd0d2bbcf950f517e5cb6a1d6453815992
Paul McKellar <paul.mckellar@gmail.com>
2012-06-20 11:51:43 -0700

    passes all specs

--- lib/shell/binary_checker.rb
+++ lib/shell/binary_checker.rb
@@ -4,11 +4,8 @@ class BinaryChecker
   end
 
   def exist?
-    # would love something better
-    # out = `command -v #{@command} >/dev/null 2>&1`
-    # $?.to_s.scan("0") != []
-    !ENV['PATH'].split(':').
-        each {|folder| puts File.exists?(folder+'/'+@command)}.
+    !!ENV['PATH'].split(':').
+        map {|folder| File.exists?(folder+'/'+@command)}.
         inject(false){|a,b| a || b }
   end
 end
